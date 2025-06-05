4461a1cf2c3633fb4b55c0442d54dbed4db36cd1
Alifiansyah Firdaus <alifiansyahfkp@gmail.com>
2025-06-05 21:14:55 +0700

    Remove: raw json log output

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ async function main() {
         const json = await events.json();
         const eventJson = json as Array<ActivityEvent>;
         
-        console.log(eventJson);
         eventJson.forEach(event => {
             console.log(getActivityList(event))
         });
